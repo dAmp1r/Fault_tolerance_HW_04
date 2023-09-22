2848b8cb5d52e7fae62f346f10200bb6b3352865
dAmp1r <139043967+dAmp1r@users.noreply.github.com>
2023-09-22 15:50:06 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,97 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
+resource "yandex_compute_instance" "vm" {
+  count = 2
+  name = "vm${count.index}"
+
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8a67rb91j689dqp60h"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  
+  metadata = {
+    user-data = "${file("/Users/damp1r/netology/yc-cloud/meta.txt")}"
+  }
+
+}
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_lb_target_group" "my-target" {
+  name      = "my-target"
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    address   = yandex_compute_instance.vm[0].network_interface.0.ip_address
+  }
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    address   = yandex_compute_instance.vm[1].network_interface.0.ip_address
+  }
+
+}
+
+resource "yandex_lb_network_load_balancer" "balancer" {
+  name = "balancer"
+  listener {
+    name = "balancer"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.my-target.id
+    healthcheck {
+      name = "http"
+        http_options {
+          port = 80
+          path = "/"
+        }
+    }
+  }
+}
+
+output "internal_ip_address_vm-0" {
+  value = yandex_compute_instance.vm[0].network_interface.0.ip_address
+}
+output "external_ip_address_vm-0" {
+  value = yandex_compute_instance.vm[0].network_interface.0.nat_ip_address
+}
+output "internal_ip_address_vm-1" {
+  value = yandex_compute_instance.vm[1].network_interface.0.ip_address
+}
+output "external_ip_address_vm-1" {
+  value = yandex_compute_instance.vm[1].network_interface.0.nat_ip_address
+}
